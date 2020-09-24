dcb43ae5b023b3bc882a1ea3d22f730314eaa810
shuker <shuker@foxmail.com>
2020-09-24 17:24:59 +0800

    聚合Swagger接口文档

--- axbx-admin-ums/src/main/java/net/sk32/axbx/ums/AxbxAdminUmsApplication.java
+++ axbx-admin-ums/src/main/java/net/sk32/axbx/ums/AxbxAdminUmsApplication.java
@@ -2,7 +2,9 @@ package net.sk32.axbx.ums;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
 @SpringBootApplication
 public class AxbxAdminUmsApplication {
 
---
+++ axbx-gateway/src/main/java/net/sk32/axbx/gateway/config/GwSwaggerResourceProvider.java
@@ -0,0 +1,62 @@
+package net.sk32.axbx.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.support.NameUtils;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * 聚合各个服务的 Swagger 文档接口
+ */
+@Slf4j
+@Primary
+@Component
+public class GwSwaggerResourceProvider implements SwaggerResourcesProvider {
+    public static final String API_URI = "/v3/api-docs";
+
+    /**
+     * 网关路由
+     */
+    @Autowired
+    private RouteLocator routeLocator;
+
+    @Autowired
+    private GatewayProperties gatewayProperties;
+
+    /**
+     * Gets a result.
+     *
+     * @return a result
+     */
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        List<String> routes = new ArrayList<>();
+        routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
+        gatewayProperties.getRoutes().stream()
+                .filter(routeDefinition -> routes.contains(routeDefinition.getId()))
+                .forEach(routeDefinition -> routeDefinition.getPredicates().stream()
+                        .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
+                        .forEach(predicateDefinition -> resources.add(swaggerResource(routeDefinition.getId(),
+                                predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0").replace("/**", API_URI)))));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion("3.0");
+        return swaggerResource;
+    }
+}
